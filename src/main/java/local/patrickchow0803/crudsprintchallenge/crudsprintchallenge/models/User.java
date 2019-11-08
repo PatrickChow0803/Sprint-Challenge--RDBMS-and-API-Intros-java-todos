6bcaa41acc3a5bdba418d1f269904bc2ac54fe27
Patrick Chow <PatrickChow0803@gmail.com>
2019-11-08 12:04:52 -0500

    Added the join tables.

--- src/main/java/local/patrickchow0803/crudsprintchallenge/crudsprintchallenge/models/User.java
+++ src/main/java/local/patrickchow0803/crudsprintchallenge/crudsprintchallenge/models/User.java
@@ -1,6 +1,8 @@
 package local.patrickchow0803.crudsprintchallenge.crudsprintchallenge.models;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
@@ -21,6 +23,15 @@ public class User {
     @Column(nullable = false)
     private String password;
 
+    @ManyToMany
+    @JoinTable(name = "userroles",
+    joinColumns = @JoinColumn(name = "userid"),
+    inverseJoinColumns = @JoinColumn(name = "roleid"))
+    List<Role> roles = new ArrayList<>();
 
+    @OneToMany(mappedBy = "user",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true)
+    List<Todo> todos = new ArrayList<>();
 
 }
