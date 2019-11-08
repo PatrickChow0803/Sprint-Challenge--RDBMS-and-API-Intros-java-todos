533a44fc8a37d12af3a947a31dac88fc40f5a4a9
Patrick Chow <PatrickChow0803@gmail.com>
2019-11-08 11:15:53 -0500

    Patrick Chow: Initial Commit. Added Dependencies, changed application properties, and added SeedData file

---
+++ src/main/java/local/patrickchow0803/crudsprintchallenge/crudsprintchallenge/CrudSprintChallengeApplication.java
@@ -0,0 +1,13 @@
+package local.patrickchow0803.crudsprintchallenge.crudsprintchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSprintChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudSprintChallengeApplication.class, args);
+    }
+
+}
